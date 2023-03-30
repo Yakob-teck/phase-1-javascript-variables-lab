bb70c497ba25b3730fdd084fa761bcc90d9a8f40
asebeyakob@gmail.com <asebeyakob@gmail.com>
2023-03-30 12:30:11 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Code your solution in this file!
+
+companyName;
+const companyName ="Scuber";
+mostProfitableNeighborhood;
+   let mostProfitableNeighborhood='Chelsea';
+  let companyCeo='Susan Smith';
